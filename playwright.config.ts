7a5d4d04ca7b212e438a632fb6fd9f36a3002665
Raghu <raghuvardhan.qa@gmail.com>
2026-03-08 19:44:17 -0400

    Add sharding CI pipeline and fix missing BASE_URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL ?? 'https://ecommerce-playground.lambdatest.io',
 
         /* Records traces after each test failure for debugging purposes. */
     trace: 'retain-on-failure',
